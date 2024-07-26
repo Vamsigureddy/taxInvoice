5cf0dfe8ca859a2ead974f8853e820850c96c221
Vamsigureddy <vamsikrishnareddy143.vr@gmail.com>
2024-07-26 21:13:40 +0530

    video add in home screen

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
+  constructor(private router: Router) {}
 
+  userDetails(){
+    this.router.navigate(['/angular-training']);
+    }
+    @ViewChild('contactButton') contactButton: ElementRef;
+
+  onMouseEnter() {
+    this.contactButton.nativeElement.classList.remove('btn-danger');
+    this.contactButton.nativeElement.classList.add('btn-success');
+  }
+
+  onMouseLeave() {
+    this.contactButton.nativeElement.classList.remove('btn-success');
+    this.contactButton.nativeElement.classList.add('btn-primary');
+  }
 }
